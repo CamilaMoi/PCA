fd8c9fc790697d8e47dcebad3f5f5368380aeed8
CamilaMoi <120430629+CamilaMoi@users.noreply.github.com>
2023-02-09 00:28:34 -0300

    Update 20230126_004.c

--- 20230126/20230126_004.c
+++ 20230126/20230126_004.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int matriz[3][3];
-    int x;
-    int linha, coluna;
-    int encontrado = 0;
+    int matriz[3][3], int x, int linha, coluna, int encontrado = 0;
 
     printf("Digite os valores da matriz 3 x 3: \n");
     for (linha = 0; linha < 3; linha++) {
@@ -18,17 +15,10 @@ int main() {
 
     for (linha = 0; linha < 3; linha++) {
         for (coluna = 0; coluna < 3; coluna++) {
-            if (matriz[linha][coluna] == x) {
-                encontrado = 1;
-                break;
-            }
-        }
-        if (encontrado) break;
-    }
-
-    if (encontrado) {
+            if (matriz[linha][coluna] == x){
         printf("Valor encontrado na posição [%d][%d]\n", linha, coluna);
-    } else {
+            }
+            else {
         printf("Valor não encontrado\n");
     }
 
