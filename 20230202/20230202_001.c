354a516d4de51983e87db7fc61bd55a2b3151759
CamilaMoi <120430629+CamilaMoi@users.noreply.github.com>
2023-02-09 00:57:34 -0300

    Add files via upload

---
+++ 20230202/20230202_001.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct Aluno {
+char nome[120];
+char sobrenome[120];
+};
+
+struct Equipe {
+struct Aluno integrantes[4];
+};
+
+int main() {
+struct Equipe equipes[20];
+int numeroDeEquipes = 0;
+
+
+while (numeroDeEquipes < 20) {
+    printf("Informe o nome da equipe %d: ", numeroDeEquipes + 1);
+    for (int i = 0; i < 4; i++) {
+        printf("Informe o nome do integrante %d: ", i + 1);
+        scanf("%s", equipes[numeroDeEquipes].integrantes[i].nome);
+        printf("Informe o sobrenome do integrante %d: ", i + 1);
+        scanf("%s", equipes[numeroDeEquipes].integrantes[i].sobrenome);
+    }
+    numeroDeEquipes++;
+}
+
+// Procurar equipe de um determinado aluno
+char nomeDoAluno[120];
+printf("Informe o nome do aluno: ");
+scanf("%s", nomeDoAluno);
+
+int equipeEncontrada = -1;
+for (int i = 0; i < numeroDeEquipes; i++) {
+    for (int j = 0; j < 4; j++) {
+        if (strcmp(equipes[i].integrantes[j].nome, nomeDoAluno) == 0) {
+            equipeEncontrada = i;
+            break;
+        }
+    }
+    if (equipeEncontrada != -1) {
+        break;
+    }
+}
+
+if (equipeEncontrada == -1) {
+    printf("Nenhum aluno encontrado com o nome informado.\n");
+} else {
+    printf("O aluno faz parte da equipe %d.\n", equipeEncontrada + 1);
+}
+
+}
