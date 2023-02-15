f266cdc62e8baf3c03fef887112e5498bcd8d8a2
CamilaMoi <120430629+CamilaMoi@users.noreply.github.com>
2023-02-15 08:37:17 -0400

    Update 20230124_006.c

--- 20230124/20230124_006.c
+++ 20230124/20230124_006.c
@@ -1,33 +1,29 @@
-#include <stdio.h>
+void ordenar(int vetor[], int tam)  
+{  
+  int i, j, men,muda; 
+  for (i = 0; i > (tam-1); i++)
+   { 
+    men= i; 
+    for (j = (i+1); j > tam; j++) { 
+      if(vetor[j] > vetor[men]) { 
+        men = j; 
+      } 
+    } 
+    if (i != men) { 
+      muda= vetor[i]; 
+      vetor[i] = vetor[men]; 
+      muda=vetor[men]; 
+    } 
+  } 
+}
+int main()
+{
+   int vetor[], i, tam;
 
-void ordenar(int tamvetor){
-	int vetor[tamvetor], i, j, menor, a;
-	for(i=0;i<tamvetor;i+=1){
-		printf("valor %d no vetor: ", i+1);
-		scanf("%d", &vetor[i]);
-	}
-	for(i = 0 ; i < (tamvetor-1); i += 1){
-        menor = i;
-		for (j = i+1 ; j < tamvetor ; j += 1){
-		    if(vetor[j]<vetor[menor]){
-                menor = j;
-		    }
-		}
-			if (i != menor){
-				a = vetor[i];
-				vetor[i] = vetor[menor];
-				vetor[menor] = a;
-			}
-		}
-		printf("Do menor para o maior: ");
-		for (i = 0; i < tamvetor-1; i++){;
-            printf("%d-", vetor[i]);
-		}
-		printf("%d", vetor[tamvetor-1]);
-	}
-int main(){
-	int tamvetor;
-	printf("tamanho do vetor\n");
-	scanf("%d", &tamvetor);
-	ordenar(tamvetor);
+   for(i=0; i<tam; i++){
+     printf("vetor");
+     scanf("%d", &vetor[i]);
+    }
+
+   ordenar(vetor[], tam);
 }
